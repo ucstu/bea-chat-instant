cf90a53447bc3eac15a3500573c803389b88e10e
张旭东 <2219454275@qq.com>
2022-10-19 14:32:12 +0000

    quit mask

--- packages/frontend/src/pages/Set.tsx
+++ packages/frontend/src/pages/Set.tsx
@@ -1,5 +1,6 @@
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ItemCard from "../components/ItemCard";
 
@@ -21,22 +22,59 @@ const itemArr = [
   },
 ];
 export default function set() {
+  const [isShow, setIsShow] = useState(false);
+  const nodeRef = useRef(null);
+  const maskNode = useRef(null);
+  function clickQuit() {
+    const show = !isShow;
+    setIsShow(show);
+    (nodeRef.current as unknown as HTMLElement).style.backgroundColor =
+      "#e6e3e3";
+    (maskNode.current as unknown as HTMLElement).style.display = "block";
+  }
+  function cancelQuit() {
+    (maskNode.current as unknown as HTMLElement).style.display = "none";
+  }
   return (
-    <div>
+    <div className="relative" style={{ height: "100vh", width: "100vw" }}>
       <Title />
       <ItemCard configure={itemArr} />
       <div className="w-full text-center absolute bottom-0">
         <button
           className="w-full h-40 mt-5"
           style={{ border: 0, padding: 0, backgroundColor: "#fdfdfd" }}
+          disabled
         >
           切换账号
         </button>
         <div
-          className="w-full h-40 mt-5 "
+          className="w-full h-40 mt-5 leading-40 "
           style={{ backgroundColor: "#fdfdfd" }}
+          onClick={clickQuit}
+          ref={nodeRef}
         >
           退出
+        </div>
+      </div>
+      {/* {quit遮罩层 弹出选项框框} */}
+      <div style={{ display: "none" }} ref={maskNode}>
+        <div
+          className="absolute h-full w-full bottom-0 "
+          style={{ backgroundColor: "#eee", opacity: 0.5 }}
+        ></div>
+        <div
+          className="absolute  leading-40 w-full text-center bottom-0 "
+          style={{
+            fontWeight: "bold",
+            backgroundColor: "#fdfdfd",
+            borderRadius: "15px 15px 0 0",
+          }}
+        >
+          <div>
+            <div>退出登录</div>
+            <div>关闭微信</div>
+            <div onClick={cancelQuit}>取消</div>
+          </div>
         </div>
       </div>
     </div>
