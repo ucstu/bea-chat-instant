1b239c69c55de15f0bf346178d61a085deab79a7
张旭东 <2219454275@qq.com>
2022-10-21 08:20:30 +0000

    a

--- packages/frontend/src/pages/Contact.tsx
+++ packages/frontend/src/pages/Contact.tsx
@@ -21,26 +21,32 @@ const arr = [
 ];
 export default function Contact() {
   return (
-    <div>
+    <div style={{ height: "calc(100vh - 50px)", overflow: "scroll" }}>
       <Header
         title="联系人"
         right={<FontAwesomeIcon icon={faSearch} size="xl" />}
       />
-      <div className="" style={{ height: "calc(100vh - 100px)" }}>
+      <div style={{ marginTop: "15px" }}>
         {arr.map((item, index) => {
           return (
             <div
-              className="h-50 w-full flex pl-15"
+              className="h-12.5 w-full flex pl-4 "
               key={index}
               style={{ boxSizing: "border-box" }}
             >
               <img
                 src="https://ts1.cn.mm.bing.net/th?id=OIP-C.B6pZ8N_dG3MNAYppM-zX0AHaEo&w=316&h=197&c=8&rs=1&qlt=90&o=6&dpr=1.25&pid=3.1&rm=2"
                 alt="avatar"
-                style={{ height: "40px", width: "40px", borderRadius: "5px" }}
+                style={{
+                  height: "40px",
+                  width: "40px",
+                  borderRadius: "5px",
+
+                  alignSelf: "center",
+                }}
               />
               <div
-                className="ml-10 w-full"
+                className="ml-4 w-full"
                 style={{
                   borderBottom: "1px solid #e6e3e3",
                   lineHeight: "50px",
