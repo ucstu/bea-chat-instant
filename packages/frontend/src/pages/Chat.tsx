cbac1ca0033de481b43c477d868bcec889a12d23
张旭东 <2219454275@qq.com>
2022-10-25 12:47:42 +0000

    aaaaa

--- packages/frontend/src/pages/Chat.tsx
+++ packages/frontend/src/pages/Chat.tsx
@@ -7,8 +7,9 @@ import { Message } from "@bea-chat/api";
 import { faCaretLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import dayjs from "dayjs";
-import { useContext, useEffect, useMemo } from "react";
+import { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+
 import { useNavigate, useParams } from "react-router-dom";
 
 export default function Chart() {
@@ -18,7 +19,10 @@ export default function Chart() {
   const utils = useContext(UtilContext);
   const { userID } = useParams();
   const navigate = useNavigate();
-
+  const nodeRef = useRef(null);
+  const [isSend, setIsSend] = useState(false);
+  const [messageContent, SetMessageContent] = useState("");
+  const [position, setPosition] = useState(20);
   const userInfo = useMemo(
     () => (userID ? contacts[userID] : undefined),
     [userID, contacts]
@@ -31,39 +35,83 @@ export default function Chart() {
     }
   }, [userID]);
 
+  function MessageItem(props: any) {
+    const { content, avatar } = props;
+    console.log(props);
+
+    return (
+      <div
+        className="h-6 leading-6 absolute flex"
+        style={{ top: `${position}px` }}
+      >
+        <div className="flex h-10 leading-10 ">
+          <div className=" bg-gray-500 rounded-md">{content}</div>
+          {/* {小三角 对话框} */}
+          <div
+            className="w-0 h-0 bg-slate-400 self-center"
+            style={{
+              border: "8px solid yellow",
+              borderColor: "transparent transparent yellow yellow ",
+              borderRadius: "4px",
+              transform: "rotate(-135deg)",
+              transform: "translateY()",
+            }}
+          ></div>
+        </div>
+        <img
+          className="h-10 w-10 rounded-lg"
+          src="https://ts1.cn.mm.bing.net/th?id=OIP-C.B6pZ8N_dG3MNAYppM-zX0AHaEo&w=316&h=197&c=8&rs=1&qlt=90&o=6&dpr=1.25&pid=3.1&rm=2"
+          alt="avatar"
+        />
+      </div>
+    );
+  }
+
   return (
-    <>
+    <div className="overflow-hidden">
       <Header
         left={
           <FontAwesomeIcon icon={faCaretLeft} onClick={() => navigate(-1)} />
         }
         title={userInfo?.name || "用户"}
       />
+      <div style={{ height: "calc(100vh - 96px)", position: "relative" }}>
+        {isSend ? <MessageItem content={messageContent} /> : ""}
+      </div>
       <div>
         <button onClick={() => callUser(userID!)}>打电话</button>
         {/* {输入块} */}
         <div className="absolute bg-slate-500 bottom-0 h-12 flex w-full">
           <input
             type="text"
-            className="p-0 m-0 w-80 h-10 self-center ml-2  "
-            style={{ borderRadius: "6px" }}
+            ref={nodeRef}
+            className="p-0 m-0  h-10 self-center ml-2.5  "
+            style={{ borderRadius: "6px", width: "calc(100vw - 70px)" }}
           />
           <button
-            onClick={() =>
+            className="w-10 ml-2.5"
+            onClick={() => {
               userID &&
-              sendMessage({
-                receiverID: userID,
-                msgType: Message.msgType.Text,
-                content: "你好",
-                dateTime: dayjs().toISOString(),
-                readied: false,
-              })
-            }
+                (nodeRef as unknown as HTMLInputElement).current.value &&
+                sendMessage({
+                  receiverID: userID,
+                  msgType: Message.msgType.Text,
+                  content: (nodeRef as unknown as HTMLInputElement).current
+                    .value,
+                  dateTime: dayjs().toISOString(),
+                  readied: false,
+                });
+              setIsSend(true);
+              SetMessageContent(
+                (nodeRef as unknown as HTMLInputElement).current.value
+              );
+              (nodeRef as unknown as HTMLInputElement).current.value = "";
+            }}
           >
             发送
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
